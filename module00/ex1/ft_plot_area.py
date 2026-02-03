a2ee2e88eddfb79ce928f9148284b8a3693a0ecf
Abdelouadoud AIT EL HAJ <amwadud@users.noreply.github.com>
2026-02-03 06:26:44 +0100

    Done

---
+++ module00/ex1/ft_plot_area.py
@@ -0,0 +1,5 @@
+def ft_plot_area():
+    """Calculate the area of a plot given length and width from user input."""
+    length = int(input("Enter length: "))
+    width = int(input("Enter length: "))
+    print(f"Area: {length * width}")
