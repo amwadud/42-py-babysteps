06398285a1443448b75ad098b2bfdac4ee088136
Abdelouadoud AIT EL HAJ <amwadud@users.noreply.github.com>
2026-02-13 01:35:21 +0100

    2026-02-13

--- module01/ex5/ft_plant_types.py
+++ module01/ex5/ft_plant_types.py
@@ -44,17 +44,45 @@ class Plant:
 
 
 class Flower(Plant):
-    def __init__(self, name: str, height: int, age: int) -> None:
+    """
+    Class representing a flower, inheriting from Plant.
+    It can have additional attributes or methods specific to flowers.
+    """
+
+    def __init__(self, name: str, height: int, age: int, color: str) -> None:
         super().__init__(name, height, age)
-        print(f"Created flower: {self.get_info()}")
+        self.color = color
+
+    def bloom(self):
+        """
+        Simulate the blooming of the flower.
+        """
+        print(f"{self.name} is blooming with {self.color} petals!")
 
 
 class Tree(Plant):
-    pass
+    def __init__(
+            self, name: str, height: int,
+            age: int, trunk_diameter: int) -> None:
+        super().__init__(name, height, age)
+        self.trunk_diameter = trunk_diameter
+
+    def produce_shade(self):
+        """
+        Simulate the tree providing shade.
+        """
+        print(f"{self.name} is providing shade "
+              f"with its {self.height}cm height!")
 
 
 class Vegetable(Plant):
-    pass
+    def __iniit__(
+            self, name: str,
+            height: int, age: int,
+            harvest_season: str, nutritional_value: int) -> None:
+        super().__init__(name, height, age)
+        self.harvest_season = harvest_season
+        self.nutritional_value = nutritional_value
 
 
 if __name__ == "__main__":
