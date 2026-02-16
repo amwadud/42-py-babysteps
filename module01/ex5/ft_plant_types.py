cd8e321ab8593ec49b421f5a5ba4928ed4687c71
Abdelouadoud AIT EL HAJ <amwadud@users.noreply.github.com>
2026-02-16 04:24:04 +0000

    done on 2026-02-16

--- module01/ex5/ft_plant_types.py
+++ module01/ex5/ft_plant_types.py
@@ -5,7 +5,6 @@ class Plant:
     """
     Blueprint for a plant with a name, height, and age.
     """
-
     plans_number = 0
 
     def __init__(self, name: str, height: int, age: int) -> None:
@@ -16,31 +15,6 @@ class Plant:
         self.height = height
         self.age = age
         Plant.plans_number += 1
-        print(f"Created plant: {self.get_info()}")
-
-    def grow(self, cm: int = 1):
-        """
-        Add centimeters to the plant's height.
-        """
-        self.height += cm
-
-    def grow_older(self, days: int = 1):
-        """
-        Add days to the plant's age.
-        """
-        self.age += days
-
-    def show(self):
-        """
-        Print the plant's information to the console.
-        """
-        print(f"{self.name}: {self.height}cm, {self.age} days old")
-
-    def get_info(self) -> str:
-        """
-        Get the plant's information as a string.
-        """
-        return f"{self.name} ({self.height}cm, {self.age} days)"
 
 
 class Flower(Plant):
