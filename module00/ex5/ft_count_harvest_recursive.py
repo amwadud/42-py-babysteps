1fbaa23fdb13bf759f34702d250c869096bd9d1d
Abdelouadoud AIT EL HAJ <amwadud@users.noreply.github.com>
2026-02-20 08:48:09 +0000

    Add space after :

--- module00/ex5/ft_count_harvest_recursive.py
+++ module00/ex5/ft_count_harvest_recursive.py
@@ -1,6 +1,6 @@
 def ft_count_harvest_recursive(day=1, days=None):
     if days is None:
-        days = int(input("Days until harvest:"))
+        days = int(input("Days until harvest: "))
     if day > days:
         return
     print(f"Day {day}")
