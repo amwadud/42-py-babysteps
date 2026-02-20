032c454007377653fe823a32177297a53485f0a1
Abdelouadoud AIT EL HAJ <amwadud@users.noreply.github.com>
2026-02-20 08:37:43 +0000

    Change printed string in case of unknwon unit type.

--- module00/ex7/ft_seed_inventory.py
+++ module00/ex7/ft_seed_inventory.py
@@ -7,4 +7,4 @@ def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     elif unit == "area":
         print(f"{seed_type} seeds: covers {quantity} square meters")
     else:
-        print(f"{seed_type} seeds: {quantity} {unit} (unknown unit)")
+        print("Unknown unit type")
