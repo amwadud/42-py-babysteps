17b9be156aae958ab77943555c63172e56325da9
Abdelouadoud AIT EL HAJ <amwadud@users.noreply.github.com>
2026-02-20 08:42:07 +0000

    Change range value for better readability.

--- module00/ex5/ft_count_harvest_iterative.py
+++ module00/ex5/ft_count_harvest_iterative.py
@@ -1,4 +1,4 @@
 def ft_count_harvest_iterative():
     days = int(input("Days until harvest:"))
-    for day in range(days):
-        print(f"Day {day + 1}")
+    for day in range(1, days + 1):
+        print(f"Day {day}")
