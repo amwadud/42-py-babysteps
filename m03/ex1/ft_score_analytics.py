4802bbad50dc261cd4f27d73a7f5ca0ac03cd0e9
Abdulwadud Ait El Hajj <amwadud@users.noreply.github.com>
2026-03-04 05:32:54 +0000

    (FIX): Rename from module0X to m0X

---
+++ m03/ex1/ft_score_analytics.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# Authorized: sys.argv, len(), sum(), max(), min(), int(), print(), try/except
+
+import sys
+
+
+def usage() -> None:
+    print(
+        "No scores provided. "
+        + " Usage: python3 ft_score_analytics.py <score1> <score2> ..."
+    )
+
+
+def display_player_score_analytics() -> None:
+    print("=== Player Score Analytics ===")
+    scores: list[int] = []
+    try:
+        scores = [int(x) for x in sys.argv[1:]]
+    except ValueError:
+        raise ValueError("Scores should be integers only.")
+    print(f"Scores processed: {scores}")
+    print(f"Total players: {len(scores)}")
+    print(f"Average score: {float(sum(scores)) / len(scores)}")
+    print(f"High score: {max(scores)}")
+    print(f"Low score: {min(scores)}")
+    print(f"Score range: {max(scores) - min(scores)}")
+
+
+def main() -> None:
+    if len(sys.argv) == 1:
+        usage()
+        return
+    try:
+        display_player_score_analytics()
+    except ValueError as error:
+        print(f"[ERROR]: {error}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ m03/ex2/ft_coordinate_system.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Authorized: import sys, sys.argv, import math, tuple(), int(), float(), print(), split(), try/except, math.sqrt()
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
