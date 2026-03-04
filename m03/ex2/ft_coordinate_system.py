2c84f7af025e77383305f0dc915c220b81967e67
Abdulwadud Ait El Hajj <amwadud@users.noreply.github.com>
2026-03-04 08:36:25 +0000

    (Update): Wed Mar  4 08:36:25 AM +00 2026

--- m03/ex2/ft_coordinate_system.py
+++ m03/ex2/ft_coordinate_system.py
@@ -1,10 +1,84 @@
 #!/usr/bin/env python3
 
-# Authorized: import sys, sys.argv, import math, tuple(), int(), float(), print(), split(), try/except, math.sqrt()
+# Authorized: import sys, sys.argv, import math, tuple(),
+# int(), float(), print(), split(), try/except, math.sqrt()
+
+from multiprocessing import Value
+from typing import override
+from math import sqrt
+
+
+class CoordinatesError(Exception):
+    pass
+
+
+class Position:
+    x: int
+    y: int
+    z: int
+
+    def __init__(
+        self, coords: tuple[int, int, int], creation_str: str = ""
+    ) -> None:
+        self.x = coords[0]
+        self.y = coords[1]
+        self.z = coords[2]
+        print(creation_str, end="")
+
+    def set(self, coords: tuple[int, int, int]) -> None:
+        self.x = coords[0]
+        self.y = coords[1]
+        self.z = coords[2]
+
+    def get(self) -> tuple[int, int, int]:
+        return (self.x, self.y, self.z)
+
+    @override
+    def __str__(self) -> str:
+        return str((self.x, self.y, self.z))
+
+    # def pars(self) -> str:
+    #     return f'"{self.x},{self.y},{self.z}"'
+
+    def dist(self, coords: tuple[int, int, int]) -> float:
+        return sqrt(
+            (coords[0] - self.x) ** 2
+            + (coords[1] - self.y) ** 2
+            + (coords[2] - self.z) ** 2
+        )
+
+
+def create_pos(x: int = 0, y: int = 0, z: int = 0) -> Position:
+    p = Position((x, y, z))
+    print("Position created: ")
+    return p
+
+
+def game_coordinate_system() -> None:
+    print("=== Game Coordinate System ===")
+    try:
+        print()
+        _ = Position((10, 20, 5), "Position created: (10, 20, 5)\n")
+        print(
+            "Distance between (0, 0, 0) and (10, 20, 5): "
+            + f"{Position((0, 0, 0)).dist((10, 20, 5))}"
+        )
+        print()
+        print('Parsing coordinates: "3,4,0"')
+        print(f"Parsed position: {Position((3, 4, 0))}")
+        print(
+            f"Distance between {Position((0, 0, 0))} and "
+            + f"{Position((3, 4, 0))}: {Position((3, 4, 0)).dist((0, 0, 0))}"
+        )
+        print()
+        print('Parsing invalid coordinates: "abc,def,ghi"')
+    except ValueError as error:
+        print(f"Error parsing coordinates: {error}")
+        print(f"Error details - Type: ValueError, Args: ({error},)")
 
 
 def main() -> None:
-    pass
+    game_coordinate_system()
 
 
 if __name__ == "__main__":
